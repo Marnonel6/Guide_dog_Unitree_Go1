4353bc56adf9bb20b99ad0af63bd9ec088047ea7
Marno (Mathinus) Nel <marthinusnel2023@u.northwestern.edu>
2023-03-03 11:37:02 -0600

    Added launch configuration's

--- guide_dog_unitree_go1/launch/guide_dog.launch.py
+++ guide_dog_unitree_go1/launch/guide_dog.launch.py
@@ -3,21 +3,41 @@ from launch_ros.actions import Node
 from launch.actions import DeclareLaunchArgument, IncludeLaunchDescription
 from launch.substitutions import LaunchConfiguration, PathJoinSubstitution
 from launch_ros.substitutions import FindPackageShare
+from launch.conditions import IfCondition
 from launch.launch_description_sources import PythonLaunchDescriptionSource
 
 
 def generate_launch_description():
     return LaunchDescription([
         DeclareLaunchArgument(
-            name='use_rviz',
+            name='use_nav2',
             default_value='true',
             choices=['true','false'],
-            description='Open RVIZ for Go1 visualization'
+            description='Runs Nav2 for autonomous navigation'
+        ),
+
+        DeclareLaunchArgument(
+            name='use_voice_control',
+            default_value='true',
+            choices=['true','false'],
+            description='Enables voice control of unitree Go1'
         ),
 
         Node(
             package='guide_dog_unitree_go1',
             executable='voice_control',
-            output='screen'
+            output='screen',
+            condition=IfCondition(LaunchConfiguration('use_voice_control')),
+        ),
+
+        IncludeLaunchDescription(
+            PythonLaunchDescriptionSource(
+                PathJoinSubstitution([
+                    FindPackageShare('unitree_nav'),
+                    'launch',
+                    'unitree_nav.launch.py'
+                ])
+            ),
+            condition=IfCondition(LaunchConfiguration('use_nav2')),
         ),
     ])
